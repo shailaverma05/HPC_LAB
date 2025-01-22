4743519bfeecbcf47b28afc6dbf11e8ebb4fb48c
shailaverma05 <130858891+shailaverma05@users.noreply.github.com>
2025-01-22 09:57:34 +0530

    Update program7.c

--- program7.c
+++ program7.c
@@ -1,21 +1,26 @@
-#include <mpi.h>
 #include <stdio.h>
-
-int main(int argc, char **argv)
+#include <stdlib.h>
+#include <mpi.h>
+int main(int argc, char *argv[])
 {
-    int rank, size, data, sum;
-
+    int size, rank;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    data = rank + 1; // Assign data as rank + 1
-    printf("Process %d has data %d\n", rank, data);
-
-    MPI_Allreduce(&data, &sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-
-    printf("Process %d: Total sum is %d\n", rank, sum);
-
+    float recvbuf, sendbuf[100];
+    if (rank == 0)
+    {
+        int i;
+        printf("Before Scatter : sendbuf of rank 0 : ");
+        for (i = 0; i < size; i++)
+        {
+            srand(i);
+            sendbuf[i] = (float)(rand() % 1000) / 10;
+            printf("%.1f ", sendbuf[i]);
+        }
+        printf("\nAfter Scatter :\n");
+    }
+    MPI_Scatter(sendbuf, 1, MPI_FLOAT, &recvbuf, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    printf("rank= %d Recvbuf: %.1f\n", rank, recvbuf);
     MPI_Finalize();
-    return 0;
 }
